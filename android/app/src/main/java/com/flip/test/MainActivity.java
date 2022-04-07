91b0343973df44e7f578842d118a90ee72eec067
rakamahardika <rakamahardika22@gmail.com>
2022-04-07 17:26:31 +0700

    Change Name

--- android/app/src/main/java/com/flip/test/MainActivity.java
+++ android/app/src/main/java/com/flip/test/MainActivity.java
@@ -1,4 +1,4 @@
-package com.suitmediatest;
+package com.flip.test;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "suitmediaTest";
+    return "FlipTest";
   }
 }
--- ios/FlipTestTests/FlipTestTests.m
+++ ios/FlipTestTests/FlipTestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface suitmediaTestTests : XCTestCase
+@interface FlipTestTests : XCTestCase
 
 @end
 
-@implementation suitmediaTestTests
+@implementation FlipTestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
